1f117e36a23872800218d27be3d8c8d923ae98a4
arty gvozdenkov <gvozdenkov@gmail.com>
2023-08-10 13:19:31 +0300

    feat: localize to EN

--- doczrc.js
+++ doczrc.js
@@ -1,6 +1,6 @@
 export default {
     typescript: true,
-    base: '/react-developer-burger-ui-components/docs',
+    base: '/react-developer-burger-ui-components',
     dest: '/docs',
     themeConfig: {
         initialColorMode: 'dark',
