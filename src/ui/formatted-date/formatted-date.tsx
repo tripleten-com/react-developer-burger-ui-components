288cdcaa94cb4722d7787df05e0cd08eec2766cb
arty gvozdenkov <gvozdenkov@gmail.com>
2023-08-14 17:31:51 +0300

    feat: today\yesterday date translate

--- src/ui/formatted-date/formatted-date.tsx
+++ src/ui/formatted-date/formatted-date.tsx
@@ -6,11 +6,11 @@ export const FormattedDate: React.FC<{
     className?: string;
 }> = ({ date, className }) => {
     if (isToday(date)) {
-        return <span className={className}>Сегодня, {getFormattedTime(date)}</span>;
+        return <span className={className}>Today, {getFormattedTime(date)}</span>;
     }
 
     if (isYesterday(date)) {
-        return <span className={className}>Вчера, {getFormattedTime(date)}</span>;
+        return <span className={className}>Yesterday, {getFormattedTime(date)}</span>;
     }
 
     return <span className={className}>{getFormattedDate(date)}</span>;
